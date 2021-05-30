9b9c0ca4d7a87fe32824f5e768e96195659e9fd9
robin <emeka14real@gmail.com>
2021-05-30 08:14:55 +0100

    Initial Commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Nnaemeka Igboanyika on 30/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

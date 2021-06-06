c510f145b08d3a836480708236be6f38acd45f08
robin <emeka14real@gmail.com>
2021-06-06 09:52:27 +0100

    Dice roller

--- StarWars/ViewController.swift
+++ StarWars/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leftDice: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func throwDiceClicked(_ sender: UIButton) {
+        let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        leftDice.image = allDice.randomElement()
+        rightImageView.image = allDice.randomElement()
+    }
+    
 }
 
